2375b39ef1b8510c5c900063a0677fb5513b08ae
Tyler Lartonoix <tyler@lartonoix.com>
2021-07-22 23:15:52 -0400

    Add missing trailing newline

--- install.sh
+++ install.sh
@@ -89,3 +89,4 @@ sh -c 'curl --silent -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autol
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim +'PlugInstall --sync' +qa
+
