fb53677410c7a0b6eefbe0fd6ad4aaeeee68a0d9
Tyler Lartonoix <lord.codemonkey@gmail.com>
2021-07-23 16:15:35 -0400

    Add PHP related abbreviations file

---
+++ configs/shell/.config/fish/conf.d/php.fish
@@ -0,0 +1,6 @@
+abbr -a composer 'sudo docker-compose exec php composer'
+abbr -a co 'sudo docker-compose exec php composer'
+
+abbr -a symfony 'sudo docker-compose exec php bin/console'
+abbr -a sym 'sudo docker-compose exec php bin/console'
+
