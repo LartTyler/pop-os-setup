b6d29b10b0fffa14656f21a602acbfb2ae4216a1
Tyler Lartonoix <tyler@lartonoix.com>
2021-07-22 22:58:53 -0400

    Add basic completions for common Docker commands
    Also begin adding completions for Composer / Symfony inside of
    Docker containers.

---
+++ configs/shell/.config/fish/conf.d/docker.fish
@@ -0,0 +1,9 @@
+abbr -a co 'sudo docker-compose exec php composer'
+
+abbr -a dc 'sudo docker-compose'
+abbr -a dce 'sudo docker-compose exec'
+abbr -a dcu 'sudo docker-compose up -d'
+
+# Completions for Composer in Docker
+set -l composer_commands install update remove
+
